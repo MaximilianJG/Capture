2ea61ef6a39a1073f47021c4d582d9b05414ef29
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-24 20:19:19 +0200

    Landing page picture via cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :sources, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_one_attached :photo
+
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
