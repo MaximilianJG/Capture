255787041c050a8baf844cfd031ad06c69d93ec8
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-24 15:37:39 +0200

    validations user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :folders, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+
+
 end
