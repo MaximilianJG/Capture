39d29cb5ba8eb6cb9e0a4b2ab664e48ac6a9c87d
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-24 15:57:44 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :folders, dependent: :destroy
   has_many :sources, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
