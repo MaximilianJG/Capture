144f3897650333a3bbe05e23310918065c21d76c
Marc Gaetcke <marc_onthemove@hotmail.com>
2020-08-25 14:21:39 +0200

    Pundit setup done + index method in source controller

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,8 +1,28 @@
 class SourcesController < ApplicationController
+  before_action :set_source, only: [:show]
 
+  # READ
   def index
     @sources = Source.all
+
+    # only show sources that the user has created himself (e.g. record.user == user)
+    @source = policy_scope(Source).all
   end
 
+  def show
+  end
 
+  # CREATE
+
+  def new
+  end
+
+  # UPDATE
+
+  private
+
+  def set_source
+    @source = Source.find(params[:id])
+    authorize @source
+  end
 end
