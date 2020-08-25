0db35241f2f219f6225691fb5d8a5bb6aee111a8
Tim Jenner <timothy.jenner.biz@gmail.com>
2020-08-25 16:09:53 +0200

    created pundit authorization for quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,11 +2,13 @@ class QuotesController < ApplicationController
 
   def new
     @quote = Quote.new
+    authorize @quote
   end
 
   def create
     # When building the chrome extension the $ goes away and use the @ sign. and after the create sources just call create a quote (or keep it the same)
     $quote = Quote.new(strong_quote_params)
+    authorize $quote
     @source = Source.all.find { |source| source.url_of_website == $quote.url_of_quote } # chanegs sources.all -> to only sources of current user
     if @source
       $quote.source_id = @source.id
