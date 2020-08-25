667a8daa843551ba0dc021a1bd04856e91516638
Tim Jenner <timothy.jenner.biz@gmail.com>
2020-08-25 18:52:43 +0200

    all fixed

--- app/policies/source_policy.rb
+++ app/policies/source_policy.rb
@@ -10,7 +10,6 @@ class SourcePolicy < ApplicationPolicy
   end
 
   def show?
-    return true
     record.user == user || user.admin
   end
 
@@ -23,17 +22,14 @@ class SourcePolicy < ApplicationPolicy
   end
 
   def edit?
-    true
     record.user == user || user.admin
   end
 
   def update?
-    true
     record.user == user || user.admin
   end
 
   def destroy?
-    true
     record.user == user || user.admin
   end
 
