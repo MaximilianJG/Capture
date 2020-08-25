a41860f913c491e5cda1f72f15c1670b43a52074
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-25 16:01:32 +0200

    You can go into the folders now

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,14 +1,8 @@
 class FoldersController < ApplicationController
 
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
   def show
+    @folder = Folder.find(params[:id])
+
+    @sources = Source.all.select { |source| source.folder_id == @folder.id } # all has to be replaced by some pundit thing
   end
 end
