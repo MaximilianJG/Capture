ad9ba025ebd6ca7f86d48066435f635ad312a4bb
Tim Jenner <timothy.jenner.biz@gmail.com>
2020-08-26 17:48:17 +0200

    Added comments on how the home page sources will have to be sorted

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :sources, only: [:index, :new, :create, :show, :edit, :destroy]
+  resources :sources, only: [:index, :new, :create, :show, :edit, :destroy, :update]
 
   resources :quotes, only: [:new, :create]
 
