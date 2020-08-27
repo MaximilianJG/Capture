d8bd72290179b3aba287a04bd2fa260e9828348f
Tim Jenner <timothy.jenner.biz@gmail.com>
2020-08-27 18:08:51 +0200

    progress

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -6,4 +6,26 @@ class FoldersController < ApplicationController
 
     @sources = Source.where(user: current_user).select { |source| source.folder_id == @folder.id } # all has to be replaced by some pundit thing
   end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      redirect_to sources_path
+    else
+      render :new
+    end
+  end
+
+
+private
+
+def folder_params
+  params.require(:source).permit(:folder_name)
 end
+
+end
+
