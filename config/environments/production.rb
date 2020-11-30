d8a56e0ccba79a14653c346ce7c3dbb01231c8c5
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-11-30 20:42:29 +0200

    redid it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,19 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "https://capture-maximilianjg.herokuapp.com" }
+  # Settings specified here will take precedence over those in config/application.rb.
 
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
