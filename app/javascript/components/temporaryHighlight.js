4d5e290e27cba225c9f39b077d8d008c8f2c8dfc
Adam Sebesta <adam.sebesta5@gmail.com>
2021-01-29 11:03:37 +0100

    changes added from work session

--- app/javascript/components/temporaryHighlight.js
+++ app/javascript/components/temporaryHighlight.js
@@ -2,19 +2,22 @@ const temporaryHighlight = () => {
   var commentField = document.getElementById('content')
   var inputText = document.getElementById("text");
   // console.log(commentField.value)
-
-  commentField.addEventListener('focus', e => {
-    const storedText = window.getSelection().toString();
-    // console.log(storedText)
-    highlight(storedText)
-  })
-
-  window.addEventListener('click', e => {
-    let temp = document.querySelector('.temporary-highlight');
-    if (!e.target.classList.contains('comment-input-field') && temp) {
-      removeTemporaryHighlight();
-    }
-  })
+  if (commentField) {
+    commentField.addEventListener('focus', e => {
+      const storedText = window.getSelection().toString();
+      // console.log(storedText)
+      highlight(storedText)
+    })
+  }
+  if (inputText) {
+    //changed to "inputText, because function was running on all pages"
+    inputText.addEventListener('click', e => {
+      let temp = document.querySelector('.temporary-highlight');
+      if (!e.target.classList.contains('comment-input-field') && temp) {
+        removeTemporaryHighlight();
+      }
+    })
+  }
 
 }
 
@@ -24,7 +27,7 @@ function highlight(text) {
   var innerHTML = inputText.innerHTML;
   var index = innerHTML.indexOf(text);
 
-  console.log(inputText.innerText)
+  // console.log(inputText.innerText)
   if (index >= 0) {
     innerHTML = innerHTML.substring(0, index) + `<span class="temporary-highlight">` + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
     inputText.innerHTML = innerHTML;
