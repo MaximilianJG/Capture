927bba8aec5a464e1e4af132b3c5b199d377b1cd
Maximilian Glasmacher <maximilian@glasmacher.com>
2021-03-21 12:57:28 +0100

    setup new api endpoints for better convention

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,47 +1,32 @@
 class Api::V1::QuotesController < Api::V1::BaseController
 require "open-uri"
-
   def create
-    current_user = User.find(api_user_params[:user_id])
-    @quote = Quote.new(api_quote_params)
+    current_user = User.find(general_post_request_params[:user_id])
+    @quote = Quote.new(content: general_post_request_params[:quote_content], url_of_quote: general_post_request_params[:url_of_quote])
     @quote.user = current_user
     authorize @quote
     @source = Source.where(user: current_user).find { |source| source.url_of_website == @quote.url_of_quote } # refactor line 11
 
     if @source
       @quote.source_id = @source.id
+      @quote.save!
     else
-      @source = Source.new(api_source_params)
-      file = URI.open(api_photo_params[:photo_url])
-      @source.photo.attach(io: file, filename: 'test.jpg', content_type: 'image/jpg')
-      @source.folder = current_user.folders.first
+      @source = Source.new(title: general_post_request_params[:source_title], website: general_post_request_params[:website], url_of_website: general_post_request_params[:url_of_website])
       @source.user = current_user
-      @source.date_of_article = "2.Sep.2020"
+
+      file = URI.open(general_post_request_params[:source_photo_url])
+      @source.photo.attach(io: file, filename: 'test.jpg', content_type: 'image/jpg')
       @source.save!
       @quote.source = @source
+      @quote.save!
     end
       @quote.save!
       render :show
   end
 
-  def api_quote_params
-    params.require(:quote).permit(:content, :url_of_quote)
-  end #
 
-  def api_source_params
-    params.require(:source).permit(:id, :title, :website, :date_of_article, :url_of_website)
-  end
-
-  def api_comment_params
-    params.require(:comment).permit(:content)
-  end
-
-  def api_photo_params
-    params.require(:photo).permit(:photo_url)
-  end
-
-  def api_user_params
-    params.require(:user).permit(:user_id)
+  def general_post_request_params
+    params.require(:general_post_request).permit(:user_id, :quote_content, :url_of_quote, :source_title, :website, :url_of_website, :source_photo_url)
   end
 
   def render_error
--- app/controllers/api/v1/sources_controller.rb
+++ app/controllers/api/v1/sources_controller.rb
@@ -1,10 +1,32 @@
 class Api::V1::SourcesController < Api::V1::BaseController
-  def create
-    @source = Source.new(api_source_params)
 
+  def create
+    current_user = User.find(general_post_request_params[:user_id])
+    @quote = Quote.new(content: general_post_request_params[:quote_content], url_of_quote: general_post_request_params[:url_of_quote])
+    @quote.user = current_user
+    authorize @quote
+
+    @source = Source.new(title: general_post_request_params[:source_title], website: general_post_request_params[:website], url_of_website: general_post_request_params[:url_of_website])
+
+    @source.user = current_user
+    file = URI.open(general_post_request_params[:source_photo_url])
+    @source.photo.attach(io: file, filename: 'image.jpg', content_type: 'image/jpg')
+    @source.save!
+
+    @quote.source = @source
+
+    @quote.save!
+    render :show
   end
 
-  def api_source_params
-    params.require(:source).permit(:title, :date_of_article, :url_of_website) # how do we get a photo?
+  private
+
+  def general_post_request_params
+    params.require(:general_post_request).permit(:user_id, :quote_content, :url_of_quote, :source_title, :website, :url_of_website, :source_photo_url)
+  end
+
+  def render_error
+    render json: { errors: @source.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
