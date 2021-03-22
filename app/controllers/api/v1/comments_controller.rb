4ba3f6f5366f403112fae6cf2a402d57e2ade38d
Maximilian Glasmacher <maximilian@glasmacher.com>
2021-03-22 19:59:24 +0100

    set up all api's except the tags, don#t show whether we should send to source#update or tags#create)

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::CommentsController < Api::V1::BaseController
+
+  def create
+    current_user = User.find(comment_post_request_params[:user_id])
+    @quote = Quote.where(user: current_user).find_by(quote_content: comment_post_request_params[:quote_content])
+
+    @comment = Comment.new(content: comment_post_request_params[:comment])
+    @comment.quote = @quote
+    @comment.user = current_user
+    @comment.save!
+    render :show
+
+  end
+
+  def comment_post_request_params
+    params.require(:comment_post_request).permit(:user_id, :quote_content, :url_of_quote, :comment)
+  end
+
+  def render_error
+    render json: { errors: @source.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+
 end
