7cdd680971ebc37451ddeb91ae51bfe2c6dd1dec
Maximilian Glasmacher <maximilian@glasmacher.com>
2021-03-23 21:39:20 +0100

    added source controller

--- app/controllers/api/v1/sources_controller.rb
+++ app/controllers/api/v1/sources_controller.rb
@@ -22,6 +22,7 @@ class Api::V1::SourcesController < Api::V1::BaseController
 
   def update
     @source = Source.find(params[:id])
+    authorize @source
     @source.tags.delete_all
 
     @tags = []
