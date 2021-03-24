483e7322d3351b4827158ca199283b1d000566b4
Maximilian Glasmacher <maximilian@glasmacher.com>
2021-03-24 19:04:20 +0100

    fixed an error in api tags

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::QuotesController < Api::V1::BaseController
 
   def create
     current_user = User.find(snippet_post_request_params[:user_id])
-    @quote = Quote.new(content: snippet_post_request_params[:quote_content], url_of_quote: general_post_request_params[:url_of_quote])
+    @quote = Quote.new(content: snippet_post_request_params[:quote_content], url_of_quote: snippet_post_request_params[:url_of_quote])
     @quote.user = current_user
     authorize @quote
     @source = Source.where(user: current_user).find { |source| source.url_of_website == @quote.url_of_quote }
