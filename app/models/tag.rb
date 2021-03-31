6ed408c2ca8fca004fef1282ec940d4c2a6bf74a
Maximilian Glasmacher <maximilian@glasmacher.com>
2021-03-31 16:23:32 +0200

    added rewind

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,7 @@ class Tag < ApplicationRecord
   has_many :sources, through: :source_tags
   has_many :user_tags
   has_many :users, through: :user_tags
+
+  validates :name, presence: true, uniqueness: true
+
 end
